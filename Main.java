a68413bfd0bac497f2556299ae70153f48a3217f
JanusMix <janusmix2013@qq.com>
2016-10-09 14:20:32 +0800

    Now add Main.java finish my doc.

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.trs.jm.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("========================================");
+        System.out.println("Now is my first usage of IntelliJ IDEA.");
+        System.out.println("It feels alright also looks great.");
+        System.out.println("So let's enjoy this IDE in my lately days.");
+        System.out.println("Break a leg!");
+        System.out.println("========================================");
+    }
+}
